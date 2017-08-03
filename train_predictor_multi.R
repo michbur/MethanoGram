3f9c8ba798812482afa2e83c04fdf9066d84a5c6
michbur <michalburdukiewicz@gmail.com>
2017-08-03 21:33:35 +0200

    re-doing cv

--- train_predictor_multi.R
+++ train_predictor_multi.R
@@ -90,7 +90,7 @@ all_three <- intersect(as.character(conditions_dat[["Name"]]), both_mcra_rna)
 
 configureMlr(show.info = FALSE)
 
-benchmark_ngram_length <- pblapply(2L:7, function(ngram_length) {
+benchmark_ngram_length <- pblapply(2L:5, function(ngram_length) {
   lapply(c(0.25, 0.5, 1), function(feature_prop) {
     lapply(c("both", "mcra_seqs", "rna_seqs"), function(ith_seqs) {
       
@@ -175,8 +175,10 @@ benchmark_ngram_length <- pblapply(2L:7, function(ngram_length) {
                             nested_cv <- resample(learnerRF_tuned, filtered_ngrams, outer, extract = getTuneResult)
                             
                             dat <- getNestedTuneResultsOptPathDf(nested_cv) 
+
                             group_by(dat, mtry, num.trees, min.node.size) %>% 
-                              summarise(mean = mean(mse.test.mean))
+                              summarise(mean = mean(mse.test.mean)) %>% 
+                              mutate(task = ith_condition)
                           })
       
       lapply(bench_res, function(i) 
